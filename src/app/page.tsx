474904675b3390c4831c6622bce63f42a4889d79
robertthown <robertomello404@gmail.com>
2023-09-29 16:25:28 -0300

    Finalizando LP frond-end React|Next|TailWind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/Header";
 import { SectionHero } from "@/components/SectionHero";
+import { SectionServices } from "@/components/SectionServices";
 
 export default function Home() {
   return (
     <>
       <Header />
       <SectionHero />
+      <SectionServices />
     </>
   )
 }
